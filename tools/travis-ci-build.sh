78daba967ef6965912bf6993fb3baebe283a2d0f
Andrés Di Giovanni <andresdigiovanni@gmail.com>
2019-09-02 16:54:37 +0200

    Added CollectCoverage

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 echo "Executing MSBuild DLL begin command..."
-dotnet ./tools/sonar/SonarScanner.MSBuild.dll begin /o:"andresdigiovanni-github" /k:"andresdigiovanni_ADN.Graphs" /d:sonar.cs.vstest.reportsPaths="**/TestResults/*.trx" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
+dotnet ./tools/sonar/SonarScanner.MSBuild.dll begin /o:"andresdigiovanni-github" /k:"andresdigiovanni_ADN.Graphs" /d:sonar.cs.opencover.reportsPaths="./tests/ADN.Graphs.Tests/coverage.opencover.xml" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
 echo "Running build..."
 dotnet build ./src/ADN.Graphs/
 dotnet build ./tests/ADN.Graphs.Tests/
 echo "Running tests..."
-dotnet test ./tests/ADN.Graphs.Tests/ --logger:trx
+dotnet test ./tests/ADN.Graphs.Tests/ /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
 echo "Executing MSBuild DLL end command..."
 dotnet ./tools/sonar/SonarScanner.MSBuild.dll end /d:sonar.login=${SONAR_TOKEN}
